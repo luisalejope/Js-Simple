2b7843324a94bc21eca5e5f0c05d3e703575e17e
pineapplexpres <luisalejandropena425@gmail.com>
2020-10-13 00:38:53 -0500

    condicionales

---
+++ Operadores_TiposDeDatos/Js/condicionales.js
@@ -0,0 +1,15 @@
+'use strict'
+
+let numero1=8;
+let numero2=7;
+
+//condicional if
+
+//Si pasa esto
+if (numero1 > numero2) {
+    //haga esto
+    alert ("El numero "+ numero1 +" es mas grande que el numero "+ numero2);
+}else{
+    //si no, haga esto
+    alert ("El numero "+ numero1 +" es mas pequeño que el numero "+ numero2);
+}
---
+++ Operadores_TiposDeDatos/Js/tipos_de_datos.js
@@ -0,0 +1,32 @@
+'use strict'
+
+//Operadores
+let numero1=7;
+let numero2=6;
+//suma
+let suma= numero1+numero2;
+//resta
+let resta= numero1-numero2;
+//division
+let division= numero1/numero2;
+//multiplicacion
+let multiplicacion= numero1*numero2;
+//residuo
+let residuo= numero1%numero2;
+
+alert ("El resultado de la suma es: "+suma)
+alert ("El resultado de la resta es: "+resta)
+alert ("El resultado de la division es: "+division)
+alert ("El resultado de la multiplicacion es: "+multiplicacion)
+alert ("El resultado del residuo es: "+residuo)
+
+
+
+//Tipos de datos
+let num_entero = 5;
+let string = "buenas";
+let boolean = false;
+
+console.log(typeof num_entero);
+console.log(typeof string);
+console.log(typeof boolean);
