abab47327aa5ba42bf16c11ca27b9da5f68b6187
Luis Alejandro Peña Murcia <luisalejandropena425@gmail.com>
2021-01-21 20:49:46 -0500

    parametrosOpcionales

--- Javascript/Js/Funciones/parametros.js
+++ Javascript/Js/Funciones/parametros.js
@@ -1,22 +1,29 @@
 'use strict'
 //Funciones:
-//Una funcion puede obtener parametros
+//Una funcion puede obtener parametros opcionales
+//Parametros opcionales:
+/*Parametros que vienen inicializados pero no hay necesidad de pasarlos 
+cuando se invoca la funcion*/
 
-function calculadora(numero1, numero2) {
+function calculadora(numero1, numero2, mostrar = false) {
 
-    console.log("Suma: "+ (numero1+numero2));
-    console.log("Resta: "+ (numero1-numero2));
-    console.log("Multiplicacion: "+ (numero1*numero2));
-    console.log("Division: "+ (numero1/numero2));
-    console.log("***********************************");
+    if (mostrar == false) {
+        console.log("Suma: " + (numero1 + numero2));
+        console.log("Resta: " + (numero1 - numero2));
+        console.log("Multiplicacion: " + (numero1 * numero2));
+        console.log("Division: " + (numero1 / numero2));
+        console.log("***********************************");
+    }else{
+        document.write("Suma: " + (numero1 + numero2)+"<br/>");
+        document.write("Resta: " + (numero1 - numero2)+"<br/>");
+        document.write("Multiplicacion: " + (numero1 * numero2)+"<br/>");
+        document.write("Division: " + (numero1 / numero2)+"<br/>");
+        document.write("***********************************"+"<br/>");
+    }
     
+
 }
 
 
 
-for (let i = 1; i <= 10; i++) {
-    
-    console.log(i);
-    calculadora(i,5);
-
-}
+calculadora(5,2, true);
