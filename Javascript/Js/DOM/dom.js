1f6064c81db35ca2bf5386dbdc013b55b2a5402c
Luis Alejandro Peña Murcia <luisalejandropena425@gmail.com>
2021-01-24 11:52:25 -0500

    dom2

--- Javascript/Js/DOM/dom.js
+++ Javascript/Js/DOM/dom.js
@@ -2,25 +2,37 @@
 
 // DOM - Document objecto model
 
-//Obtener  elemento dentro del Div
-let caja = document.getElementById("caja").innerHTML;
+//CONSEGUIR ELEMENTOS CON UN ID CONCRETO
 
-// Obtener todo el div con query selector
-let cajota = document.querySelector("#cajota");
+        //Obtener  elemento dentro del Div
+        let caja = document.getElementById("caja").innerHTML;
 
-//Obtener todo el Div
-    caja = document.getElementById("caja");
+        // Obtener todo el div con query selector
+        let cajota = document.querySelector("#cajota");
 
-//Cambiar algo del Dom
-    caja.innerHTML="TEXTO EN LA CAJA DESDE JS";
+        //Obtener todo el Div
+            caja = document.getElementById("caja");
 
-//Cambiar el estilo
-    caja.style.background = "red";
-    caja.style.padding = "20px";
-    caja.style.color = "white";
+        //Cambiar algo del Dom
+            caja.innerHTML="TEXTO EN LA CAJA DESDE JS";
 
-function cambiaColor(color) {
-    cajota.style.background = color;
-}
+        //Cambiar el estilo
+            caja.style.background = "red";
+            caja.style.padding = "20px";
+            caja.style.color = "white";
 
-console.log(caja);
+        function cambiaColor(color) {
+            cajota.style.background = color;
+        }
+
+        console.log(caja);
+
+//CONSEGUIR ELEMENTOS POR SU ETIQUETA / Devuelve un array
+        let todosLosDiv = document.getElementsByTagName('div');
+        todosLosDiv[2].innerHTML="Cambie mi nombre"
+        console.log(todosLosDiv);
+//CONSEGUIR ELEMENTOS POR SU CLASE CSS
+
+        let divsRojos = document.getElementsByClassName('rojo');
+        divsRojos[1].style.background="red";
+
