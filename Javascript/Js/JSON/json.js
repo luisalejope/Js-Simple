fa83f70a522c6c5813a7cc9b54e880fef2981f8d
Luis Alejandro Peña Murcia <luisalejandropena425@gmail.com>
2021-01-24 22:19:55 -0500

    json

---
+++ Javascript/Js/JSON/json.js
@@ -0,0 +1,16 @@
+'use strict'
+
+// JSON - Javascript Object Notation
+
+let pelicula = {
+    titulo: 'batman vs superman',
+    year: 2017,
+    pais: 'estados unidos'
+};
+
+let peliculas = new Array(pelicula)
+
+//para acceder a una propiedad. poner pelicula.titulo
+
+pelicula.titulo="Superman begins";
+console.log(pelicula)
